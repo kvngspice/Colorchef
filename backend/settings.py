389fbbce996437623d031440aa89cfed0c7551f6
kvngspice <sampelumibiz@gmail.com>
2025-03-18 10:50:40 +0100

    first

--- backend/settings.py
+++ backend/settings.py
@@ -46,6 +46,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MIDDLEWARE = [
      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
